5f022d05cb8acb96214cbaacbc94f9b8397ba934
Kris Claessens <59504265+claessenskris@users.noreply.github.com>
2024-05-31 19:23:45 +0200

    Fix vis directory

--- exp/inference/inference.py
+++ exp/inference/inference.py
@@ -164,7 +164,7 @@ def inference(net, img_path='', output_path='./', output_name='f', use_gpu=True)
     vis_res = decode_labels(results)
 
     parsing_im = Image.fromarray(vis_res[0])
-    parsing_im.save(output_path + '_vis' + '/{}_vis.png'.format(output_name))
+    parsing_im.save(output_path + '_vis' + '/{}_vis.png'.format(output_name[:-4]))
     cv2.imwrite(output_path + '/{}.png'.format(output_name), results[0, :, :])
 
     end_time = timeit.default_timer()
